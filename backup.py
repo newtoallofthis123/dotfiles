3afdad448d922aee563c12e2e4f940dd62187793
newtoallofthis123 <noobscience123@gmail.com>
2024-05-26 11:28:04 +0530

    Update Script

--- backup.py
+++ backup.py
@@ -1,6 +1,7 @@
+#!/bin/python3
+
 import os
 
-# copy all of the files from ~/.config/i3 to cwd
 os.system("cp -r ~/.config/i3 .")
 os.system("cp -r ~/.config/i3blocks .")
 os.system("cp -r ~/.config/kitty .")
